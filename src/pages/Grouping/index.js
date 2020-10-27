c523b0f4df07209b2c69c488204be60d4f3d184d
SUCHMOKUO <suchmokuo@gmail.com>
2020-10-27 23:54:37 +0800

    [liuxing] add grouping list title style

--- src/pages/Grouping/index.js
+++ src/pages/Grouping/index.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { httpClient } from '../../api';
 import List from '../../components/List';
+import Group from '../../components/Group';
 
 import './index.scss';
 
 export default function Grouping() {
   const [students, setStudents] = useState([]);
+  const [groups, setGroups] = useState([]);
 
   const fetchStudents = () => {
     httpClient.get('/students').then((data) => {
@@ -25,6 +27,16 @@ export default function Grouping() {
 
   return (
     <div className="grouping">
+      <div className="grouping-title-container">
+        <h2>分组列表</h2>
+        <button type="button" className="grouping-button">
+          分组学员
+        </button>
+      </div>
+      {groups.map((group) => (
+        <Group key={group.name} name={group.name} data={group.data} />
+      ))}
+
       <h2>学员列表</h2>
       <List
         addable
